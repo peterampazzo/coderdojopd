498c71a7e4e528a69c4d814d5783d34e7bde25a2
Pietro Rampazzo <pietro@rampazzo.co>
2019-11-28 18:18:22 +0100

    Add card section in homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,35 +1,51 @@
 import React from "react"
-import { Layout, Row, Col, Typography } from "antd"
+import { Link } from "gatsby"
+import { Layout, Row, Col } from "antd"
+
 import Header from "../components/header"
 import Footer from "../components/footer"
 import Newsletter from "../components/newsletter"
+import Card from "../components/card"
 
 import "../style.less"
-
-const { Title } = Typography
-const { Content } = Layout
 
 export default ({ data }) => (
   <div>
     <Layout>
-      <Header />
-      <div className="about">
-        {/* <Title level={1}>{data.site.siteMetadata.title}</Title> */}
-        <img src={"/coderdojopadova.png"} alt="CoderDojo Padova" />
-      </div>
-      <Content style={{ marginTop: "20px", padding: "20px" }}>
+      <Header
+        text="Laboratorio di programmazione gratuito per bambini e ragazzi dai 5 anni
+        in su"
+      />
+      <div style={{ backgroundColor: "#F2F2F2", padding: "50px" }}>
         <Row>
           <Col span={8} align="middle">
-            <Title level={4}>Cos'è</Title>
+            <Link to="/cose/">
+              <Card
+                imgAlt=""
+                imgSrc="/img/Femo_166.jpg"
+                cardTitle="Cos'è"
+                cardDesc="CoderDojo è un movimento open source, guidato da volontari, che si occupa di creare e organizzare club di programmazione gratuiti e senza scopo di lucro per giovani."
+              />
+            </Link>
           </Col>
           <Col span={8} align="middle">
-            <Title level={4}>Partecipa</Title>
+            <Card
+              imgAlt=""
+              imgSrc="/img/Femo_197.jpg"
+              cardTitle="Partecipa"
+              cardDesc="Il nostro dojo riesce ad accogliere circa 30 bambini ad ogni appuntamento, il numero dipende dalla quantità di volontari e dalla capacità del luogo che ci ospita."
+            />
           </Col>
           <Col span={8} align="middle">
-            <Title level={4}>Stile</Title>
+            <Card
+              imgAlt=""
+              imgSrc="/img/arduino.jpg"
+              cardTitle="Stile"
+              cardDesc="A Padova, come molti altri dojo italiani, siamo molto legati al Creative Learning del MIT e alle sue 4 P: Project, Peers, Play e Passion."
+            />
           </Col>
         </Row>
-      </Content>
+      </div>
       <Newsletter />
       <Footer siteTitle={data.site.siteMetadata.title} />
     </Layout>
