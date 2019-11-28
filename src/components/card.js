90c3e23bbf370d8565d5224e5a7b1ac9101d41ee
Pietro Rampazzo <pietro@rampazzo.co>
2019-11-28 18:17:14 +0100

    Add card component

---
+++ src/components/card.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { Card } from "antd"
+
+export default props => (
+  <Card
+    hoverable
+    style={{ width: 300, borderRadius: "20px" }}
+    cover={
+      <img alt={props.imgAlt} src={props.imgSrc} style={{ height: "auto" }} />
+    }
+  >
+    <Card.Meta title={props.cardTitle} description={props.cardDesc} />
+  </Card>
+)
