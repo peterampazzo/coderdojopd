be610f90e01640eda1f57df4a3155be188ce4186
Pietro Rampazzo <pietro@rampazzo.co>
2019-11-29 11:53:41 +0100

    Align vertically newsletter items

--- src/components/newsletter.js
+++ src/components/newsletter.js
@@ -1,40 +1,41 @@
 import React from "react"
-import { Layout, Form, Icon, Input, Button, Typography } from "antd"
+import { Layout, Form, Icon, Input, Button, Typography, Col, Row } from "antd"
 
 export default props => (
-  <Layout.Content style={{ backgroundColor: "#ED684A", padding: "20px" }}>
-    <div
-      style={{
-        width: "250px",
-        marginLeft: "auto",
-        marginRight: "auto",
-        alignItems: "center",
-      }}
-    >
-      <Typography.Title
-        level={3}
-        style={{
-          textTransform: "uppercase",
-          color: "#fff",
-        }}
-      >
-        Rimani Aggiornato
-      </Typography.Title>
-      <Form>
-        <Form.Item>
-          <Input
-            prefix={<Icon type="mail" style={{ color: "rgba(0,0,0,.25)" }} />}
-            type="email"
-            placeholder="E-Mail"
-            style={{ width: "250px" }}
-          />
-        </Form.Item>
-        <Form.Item>
-          <Button type="primary" htmlType="submit">
-            Iscriviti
-          </Button>
-        </Form.Item>
-      </Form>
-    </div>
+  <Layout.Content
+    style={{
+      backgroundColor: "#ED684A",
+      padding: "20px",
+      paddingTop: "40px",
+    }}
+  >
+    <Row>
+      <Col span={24} align="middle">
+        <Typography.Title
+          level={3}
+          style={{
+            textTransform: "uppercase",
+            color: "#fff",
+          }}
+        >
+          Rimani Aggiornato
+        </Typography.Title>
+        <Form>
+          <Form.Item>
+            <Input
+              prefix={<Icon type="mail" style={{ color: "rgba(0,0,0,.25)" }} />}
+              type="email"
+              placeholder="E-Mail"
+              style={{ width: "250px" }}
+            />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit">
+              Iscriviti
+            </Button>
+          </Form.Item>
+        </Form>
+      </Col>
+    </Row>
   </Layout.Content>
 )
