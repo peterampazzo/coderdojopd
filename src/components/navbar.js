5bb25fc9d1ade200a42743bccd7da8182a5578ca
Pietro Rampazzo <pietro@rampazzo.co>
2019-12-15 19:17:19 +0100

    Add drawer

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,33 +1,70 @@
 import React from "react"
 import { Link } from "gatsby"
-import { Layout, Menu } from "antd"
+import { Layout, Menu, Drawer, Icon } from "antd"
 
-const linkStyle = {
-  textDecoration: "none",
+class Navbar extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { visible: false, placement: "left" }
+  }
+
+  showDrawer = () => {
+    this.setState({
+      visible: true,
+    })
+  }
+
+  onClose = () => {
+    this.setState({
+      visible: false,
+    })
+  }
+
+  render() {
+    const linkStyle = {
+      textDecoration: "none",
+    }
+    return (
+      <Layout.Header>
+        <div>
+          <Icon
+            type="menu"
+            onClick={this.showDrawer}
+            style={{ color: "#fff", fontSize: "20px" }}
+          />
+          <Drawer
+            title="CoderDojo Padova"
+            placement={this.state.placement}
+            closable={false}
+            onClose={this.onClose}
+            visible={this.state.visible}
+          >
+            <Menu
+              mode="inline"
+              // defaultSelectedKeys={["2"]}
+              style={{ lineHeight: "63px" }}
+            >
+              <Menu.Item key="1">
+                <Link to="/partecipa/" style={linkStyle}>
+                  Partecipa
+                </Link>
+              </Menu.Item>
+              <Menu.Item key="2">
+                <Link to="/calendario/" style={linkStyle}>
+                  Incontri
+                </Link>
+              </Menu.Item>
+              <Menu.Item key="3">
+                <Link to="/contatti/" style={linkStyle}>
+                  Contatti
+                </Link>
+              </Menu.Item>
+            </Menu>
+          </Drawer>
+        </div>
+      </Layout.Header>
+    )
+  }
 }
 
-export default () => (
-  <Layout.Header>
-    <Menu
-      mode="horizontal"
-      // defaultSelectedKeys={["2"]}
-      style={{ lineHeight: "63px" }}
-    >
-      <Menu.Item key="1">
-        <Link to="/partecipa/" style={linkStyle}>
-          Partecipa
-        </Link>
-      </Menu.Item>
-      <Menu.Item key="2">
-        <Link to="/calendario/" style={linkStyle}>
-          Incontri
-        </Link>
-      </Menu.Item>
-      <Menu.Item key="3">
-        <Link to="/contatti/" style={linkStyle}>
-          Contatti
-        </Link>
-      </Menu.Item>
-    </Menu>
-  </Layout.Header>
-)
+export default Navbar
